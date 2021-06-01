338b48e232ed0facf2d73f3b5db634e57e2f7e9a
Emran Tokhi <emrantokhiwork@gmail.com>
2021-05-31 22:12:01 -0700

    Display arithmetic operations on output screen.

---
+++ chapter1/BasicCalculator/BasicCalculator.cpp
@@ -0,0 +1,20 @@
+﻿// BasicCalculator.cpp 
+// Display basic arithemtic operators
+
+#include "BasicCalculator.h"
+
+int main()
+{
+	cout << "7 + 3 = " << 7 + 3 << endl;
+	cout << "7 - 3 = " << 7 - 3 << endl;
+	cout << "7 * 3 = " << 7 * 3 << endl;
+
+	cout << "7 / 3 = " << 7 / 3 << endl;
+	cout << "7.0 / 3.0 = " << 7.0 / 3.0 << endl;
+	cout << "7 % 3 = " << 7 % 3 << endl;
+
+	cout << "7 + 3 * 5 = " << 7 + 3 * 5 << endl;
+	cout << "(7 + 3) * 5 = " << (7 + 3) * 5 << endl;
+
+	return 0;
+}
---
+++ chapter1/BasicCalculator/BasicCalculator.h
@@ -0,0 +1,8 @@
+﻿// BasicCalculator.h 
+// Header file for basic calculator
+
+#pragma once
+
+#include <iostream>
+using std::cout;
+using std::endl;
