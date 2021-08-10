c5bf0c17a42d6782be5f2cfe29add96d766a1d0e
Emran Tokhi <emrantokhiwork@gmail.com>
2021-08-10 15:59:02 -0700

    Demonstrate passing by reference through functions

---
+++ chapter6/InventoryDisplayer/InventoryDisplayer.cpp
@@ -0,0 +1,29 @@
+﻿// InventoryDisplayer.cpp 
+// Demonstrates passing by reference for efficiency
+
+#include "InventoryDisplayer.h"
+
+void displayItems(const std::vector<std::string>& inventory);
+
+int main()
+{
+	std::vector<std::string> inventory;
+	inventory.push_back("Shield");
+	inventory.push_back("Sword");
+	inventory.push_back("Armor");
+	inventory.push_back("Potion of Healing");
+
+	displayItems(inventory);
+	return 0;
+}
+
+//Declaring const prevents any items in inventory from being changed
+void displayItems(const std::vector<std::string>& inventory) {
+	std::vector<std::string>::const_iterator cIter;
+	
+	std::cout << "Here is the inventory: " << std::endl;
+	for (cIter = inventory.begin(); cIter != inventory.end(); cIter++) {
+		std::cout << *cIter << std::endl;
+	}
+
+}
---
+++ chapter6/InventoryDisplayer/InventoryDisplayer.h
@@ -0,0 +1,9 @@
+﻿// InventoryDisplayer.h 
+// Header file for InventoryDisplayer
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
