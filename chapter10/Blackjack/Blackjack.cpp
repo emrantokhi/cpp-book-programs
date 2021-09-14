4c3098950d22b7461be3bf3dc7c3fb005ecaadbf
Emran Tokhi <emrantokhiwork@gmail.com>
2021-09-14 04:13:01 -0700

    Enabled win conditions as well as lose conditions for house and player

--- chapter10/Blackjack/Blackjack.cpp
+++ chapter10/Blackjack/Blackjack.cpp
@@ -7,6 +7,10 @@ void introduction();
 int numPlayers();
 std::vector<Player*>* setPlayers();
 bool askPlayAgain();
+bool askDraw();
+void printHouse(House* house);
+void printBoard(Player* player, House* house);
+void printWinners(std::vector<Player*>* players, House* house);
 void playBlackJack(std::vector<Player*>* players, House* house);
 
 int main()
@@ -33,19 +37,104 @@ int main()
 }
 
 void playBlackJack(std::vector<Player*>* players, House* house) {
-	bool game = true;
-	//print the board
+	bool draw = false;
+	//run through all players before exposing house
 	for (int i = 0; i < players->size(); i++) {
-		//print player score
-		std::cout << (*players)[i]->getName() << ":\n";
-		(*players)[i]->printHand();
-		std::cout << "Score: " << (*players)[i]->getScore() << "\n";
-		
-		std::cout << house->getName() << ":\n";
-		house->printHand();
-		std::cout << "\n";
+		printBoard((*players)[i], house);
+
+		draw = askDraw();
+		while (draw) {
+			(*players)[i]->draw();
+			printBoard((*players)[i], house);
+			if ((*players)[i]->getScore() == 21) {
+				(*players)[i]->setStatus("Win");
+				std::cout << "\nAwesome! You just got 21!\n\n";
+				break;
+			}
+			else if ((*players)[i]->getScore() > 21) {
+				(*players)[i]->setStatus("Bust");
+				std::cout << "\n" << (*players)[i]->getName() << ": Bust! Try again next time!\n\n";
+				break;
+			}
+			draw = askDraw();
+		}
 	}
-	game = false;
+	//all players have been ran through, expose house
+	house->revealHand();
+	//if house's score is less than or equal to 16, keep drawing
+	while (house->getScore() <= 16) {
+		house->draw();
+		printHouse(house);
+	}
+	//set status to check if bust or win
+	if (house->getScore() == 21) {
+		house->setStatus("Win");
+	}
+	else if (house->getScore() > 21) {
+		house->setStatus("Bust");
+	}
+
+	printHouse(house);
+
+	printWinners(players, house);
+}
+
+//print house only after reveal
+void printHouse(House* house) {
+	//print house
+	std::cout << house->getName() << ":\n";
+	house->printHand();
+	std::cout << "Score: " << house->getScore() << "\n\n";
+}
+
+void printWinners(std::vector<Player*>* players, House* house) {
+	bool oneWinner = false;
+	std::cout << "\nWinners:\n";
+	for (int i = 0; i < players->size(); i++) {
+		//if the player is bust or has the same score as house, continue
+		if (((*players)[i]->getStatus() == "Bust") || ((*players)[i]->getScore() == house->getScore())) {
+			continue;
+		}
+		//if players score is > house score or if house busted and player did not
+		else if(((*players)[i]->getScore() > house->getScore()) || 
+			((house->getStatus() == "Bust") && (*players)[i]->getStatus() != "Bust")) {
+			std::cout << (*players)[i]->getName() << "\n";
+			oneWinner = true;
+		}
+	}
+
+	if (!oneWinner) {
+		std::cout << "House! Everyone lost...\n";
+	}
+}
+
+void printBoard(Player* player, House * house) {
+	//print player score
+	std::cout << "--------------------------------\n";
+	std::cout << player->getName() << ":\n";
+	player->printHand();
+	std::cout << "Score: " << player->getScore() << "\n";
+	//print house
+	std::cout << "\n" << house->getName() << ":\n";
+	house->printHand();
+	std::cout << "\n";
+	std::cout << "--------------------------------\n";
+}
+
+//Ask the player if they want to draw
+bool askDraw() {
+	char choice = 'f';
+	while (choice != 'y' && choice != 'n') {
+		std::cout << "\nWould you like a hit (y/n): ";
+		std::cin >> choice;
+	}
+	std::cout << "\n";
+	if (choice == 'y') {
+		return true;
+	}
+	else {
+		return false;
+	}
 }
 
 void introduction() {
@@ -54,6 +143,7 @@ void introduction() {
 	std::cout << "Player amount: 1-7\n\n";
 }
 
+//Ask the player if they wish to play again
 bool askPlayAgain() {
 	char choice = 'f';
 	while (choice != 'y' && choice != 'n') {
@@ -80,6 +170,7 @@ int numPlayers() {
 	return players;
 }
 
+//Sets the number of players in a vector that exists on the heap
 std::vector<Player*>* setPlayers() {
 	int num_players = numPlayers();
 	//new returns a pointer, vector of Player pointers, as players will be declared on heap
